5485a6e5e98cc8de4c07d7ca4fc9cbe1ba02721f
Jeffrey Kao <jeffthestampede@gmail.com>
2013-06-29 17:45:34 -0700

    first commit w/ postgresql as db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homebase::Application.config.secret_token = 'e6a17c715a24eb6c829b2dc1c363636e7b2f04f8e71c92e7a3eb5da187f57c4a85998e42c722bc5de0d2306bc1c9dbf75bcb3efd1cc7b03268703ac865225b8e'
